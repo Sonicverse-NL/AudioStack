629c21ce5d4793d3e62df698c3784392ef5edbab
Rik Visser <rik@sonicverse.nl>
2025-07-05 01:22:40 +0200

    refactor: update harbor bind address configuration to use a list format

--- entrypoint.sh
+++ entrypoint.sh
@@ -81,7 +81,7 @@ log.stdout.set(true)
 log.file.path.set("/var/log/icecast2/liquidsoap.log")
 
 # Set harbor bind address to listen on all interfaces
-settings.server.harbor.bind_addr.set("0.0.0.0")
+set("server.harbor.bind_addresses", ["0.0.0.0"])
 
 # Fallback emergency file
 emergency = single("/etc/liquidsoap/emergency.wav")
